ca36920232a97cd3907fb6084ff2a3d0f1836c7e
Jacob <bowerman.jacob@gmail.com>
2020-10-31 14:46:45 +1000

    Gets player name and choice and seperates the players name to a function to clear up the code

--- src/penniesGame/main.java
+++ src/penniesGame/main.java
@@ -3,16 +3,34 @@ import java.io.*;
 
 public class main {
 
-	public static void main(String args[]) throws IOException {
-		String s = null;
+	public static String getPlayerInfo() throws IOException {
+		String playerName = null;
 		
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
 		
-		System.out.println("Hello, whats your name?");
-		s = br.readLine();
-		System.out.println(("Hey " + s + ", it's nice to meet you!"));
-
+		System.out.println("Welcome to Pennies. A simple text game where the aim is to not remove the last pennie in the stack.");
+		System.out.println("To start with, what is your name?");
+		playerName = br.readLine();
+		
+		System.out.println(("Hey " + playerName + ", it's nice to meet you!"));
+		
+		return playerName;
 	}
 
+	
+	public static void main(String args[]) throws IOException {
+		String playerName = null;
+		char playerChoice = 'x';
+		
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);		
+		
+		playerName = getPlayerInfo();
+		
+		System.out.println("Would you like to make the first move? (y/n)");
+		playerChoice = (char) br.read();		
+		
+
+	}
 }
